c810901daced434e2c1d7a1354b8967ef68d729d
Aswathy Santhosh <aswathysanthosh7025@gmail.com>
2026-04-14 18:02:41 +0530

    Added weather API integration

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ function addTask() {
     li.remove();
 });
 }
+async function getWeather() {
+    const city = document.getElementById("cityInput").value;
+
+    if (city.trim() === "") {
+        alert("Enter a city");
+        return;
+    }
+
+    const apiKey = "5d4495ade76f0e1bcf40a77dce3498d1";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById("weatherResult").textContent =
+            `🌡️ Temperature in ${city}: ${data.main.temp}°C`;
+    } catch (error) {
+        document.getElementById("weatherResult").textContent =
+            "Error fetching data";
+    }
+}
